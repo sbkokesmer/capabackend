1d81d9aba090e37e7c7dacd568971d82237092b4
eliffmutlu <elifmutlu174@gmail.com>
2025-08-20 04:50:34 +0300

    Fix port for Heroku

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
-// db.js
 import { Pool } from 'pg';
 
 export const pool = new Pool({
-  connectionString: 'postgresql://neondb_owner:npg_YzuC5xVf3oZK@ep-withered-feather-ad2ecejp-pooler.c-2.us-east-1.aws.neon.tech/capadental?sslmode=require'
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
